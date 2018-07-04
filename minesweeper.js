b0ed29e78d87c9048a72fda64e8ea501d999154d
D. Parker <douglasRparker@gmail.com>
2018-07-04 18:21:10 -0500

    An Updated printBoard() Function

--- minesweeper.js
+++ minesweeper.js
@@ -6,12 +6,11 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
     for (var j = 0; j < numberOfColumns; j++) {
       row.push(' ');
     } board.push(row);
-  } return board
+  } 
+
+  return board;
 
 };
-
-// console.log(generatePlayerBoard(3,3)); //BDD a player board is created;
-
 
 const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
   
@@ -32,10 +31,14 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
     board[randomRowIndex, randomColumnIndex] = 'B'
     numberOfBombsPlaced++;
     // The code in your while loop has the potential to place bombs on top of already existing bombs. This will be fixed when you learn about control flow.
-  }
+  };
+
+    const printBoard = board => {
+    console.log(board.map(row => row.join(' | ')).join('\n'));
+  }; 
 
   return board;
 
-}
+};
 
-console.log(generateBombBoard(10,10,4)); //BDD a bomb board is create with "null" values;
+console.log(generateBombBoard(5,5,3)); //BDD a bomb board is create with "null" values;
