d989fbf49fd7c51b4d7cb1b8eebbea7bf1642208
D. Parker <douglasRparker@gmail.com>
2018-07-04 01:05:25 -0500

    delete hard coded board, add array generated board

---
+++ minesweeper.js
@@ -0,0 +1,14 @@
+const printBoard = board => {
+  console.log('Current Board:');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+
+let board = [
+  [' ', ' ', ' '], 
+  [' ', ' ', ' '], 
+  [' ', ' ', ' ']
+  ];
+
+printBoard(board);
